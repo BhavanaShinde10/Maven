5af5e5f0f7158b7e5514c3ed8237b598a3e2c95c
BhavanaShinde10 <bhavanashinde60@gmail.com>
2024-03-18 18:11:37 +0530

    updated

--- src/test/java/basic/OrganizationsTest.java
+++ src/test/java/basic/OrganizationsTest.java
@@ -1,50 +1,41 @@
 package basic;
 
-import java.io.IOException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import commonutils.PropertyFileUtil;
-import commonutils.WebDriverUtil;
+
 
 public class OrganizationsTest {
 	
 	@Test
-	public void CreateOrganizationsTest() throws IOException
+	public void CreateOrganizationsTest()
 	{
-		PropertyFileUtil putil= new PropertyFileUtil();
-		WebDriverUtil wutil= new WebDriverUtil();
 		
 		
 		
 		
 			WebDriver driver= new ChromeDriver();
 			//To maximize the window
-			wutil.maximize(driver);
-			//To apply wait for findelement()
-			wutil.implicitwait(driver);
-			//driver.manage().window().maximize();
+			
+			driver.manage().window().maximize();
 			//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			//To read the data from property file
-			String URL = putil.getDataFromPropertyFile("Url");
-			String USERNAME=putil.getDataFromPropertyFile("Username");
-			String PASSWORD=putil.getDataFromPropertyFile("Password");
+			
 			//To read data from excel file
 			/*
 			 * String ORGNAME = eutil.getDataFromExcel("Organizations", 0, 1); String GROUP
 			 * = eutil.getDataFromExcel("Organizations", 1, 1);
 			 */
 			//To launch the application
-			driver.get(URL);
+			driver.get("http://localhost:8888/");
 			//To login to application
-			driver.findElement(By.name("user_name")).sendKeys(USERNAME);
-			driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+			driver.findElement(By.name("user_name")).sendKeys("admin");
+			driver.findElement(By.name("user_password")).sendKeys("admin");
 			driver.findElement(By.id("submitButton")).click();
 		
-		driver.get("http://localhost:8888/");
+		
 	}
 	
 
